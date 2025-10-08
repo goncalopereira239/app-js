0bd3d2190cfefad55ad0ba2b4420e00addd1e3ec
goncalopereira239 <goncalo.pereira239@gmail.com>
2025-10-08 10:31:39 +0100

    Criei um app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function funcJS(){
+    const vari = {
+        id: 1,
+        str: "Hello",
+        bool: true,
+        arr: [1, 2, 3],
+        obj: {
+            name: "John",
+            age: 20,
+        }
+    }
+    return vari;
+}
+
+console.log(funcJS());
